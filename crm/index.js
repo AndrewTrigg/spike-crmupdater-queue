6864c14132312d590ba00010984f1ddcbc6ea417
Andrew Trigg <atrigg@ALG3055.local>
2021-01-09 15:44:44 +0000

    Form created to initiate change of user details

--- crm/index.js
+++ crm/index.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const db = require('./db')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+app.set('view engine', 'pug')
+app.use('/static', express.static(path.join(__dirname, 'public')))
+const jsonParser = bodyParser.json()
 
 app.get('/ping', (req, res) => {
     res.send('Pong! From the crm app')
 })
 
-app.post('/hello', (req, res) => {
-    console.log('HELLLOOOOOO')
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/api', jsonParser, (req, res) => {
+    console.log(req.body)
+    // res.redirect('/')
     res.json({stuff: 'done'})
 })
 
--- platform/index.js
+++ platform/index.js
@@ -1,17 +1,29 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
+
+
+app.set('view engine', 'pug')
+app.use('/static', express.static(path.join(__dirname, 'public')))
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/ping', (req, res) => {
     res.send('Pong! From the platform app')
 })
 
-app.get('/fetch', async (req, res) => {
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.post('/', urlencodedParser, async (req, res) => {
+    console.log(req.body)
     let json
     try {
-        let resp = await fetch('http://crm:3000/hello', {
+        let resp = await fetch('http://crm:3000/api', {
             method: 'post',
-            body: JSON.stringify({a: 1}),
+            body: JSON.stringify(req.body),
             headers: { 'Content-Type': 'application/json' }
         })
         json = await resp.json()
@@ -19,7 +31,10 @@ app.get('/fetch', async (req, res) => {
     } catch (err) {
         console.log(err)
     }
-    res.send(json)
+    console.log({json})
+    res.redirect('/')
 })
+
+
 
 app.listen(4000, () => console.log('Platform started on port 4000'))
